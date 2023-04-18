68f39aa79465b9b7d27461c4821bedd3e60146ad
hossy <hossy@hossy.info>
2023-04-19 00:25:41 +0900

    Add toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,80 @@
-import React from 'react';
-import { Button } from '@fluentui/react-components';
+import React from "react";
+import {
+  Textarea,
+  Toolbar,
+  ToolbarButton,
+  ToolbarDivider,
+  Tooltip,
+  makeStyles,
+  useId,
+} from "@fluentui/react-components";
+import {
+  ArrowExpand24Regular,
+  Clipboard24Regular,
+  ClipboardPaste24Regular,
+  Form24Regular,
+  QuestionCircle24Regular,
+  SelectObject24Regular,
+  ShapeUnion24Regular,
+} from "@fluentui/react-icons";
 
-function App() {
+const useStyles = makeStyles({
+  base: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  textarea: {
+    height: "6em",
+  },
+});
+
+const App = () => {
+  const textareaId = useId("textarea");
+  const styles = useStyles();
+
   return (
-    <Button appearance="primary">Get started</Button>
+    <div className={styles.base}>
+      <Toolbar aria-label="Default">
+        <Tooltip content="Copy" relationship="description" withArrow>
+          <ToolbarButton aria-label="Copy" icon={<Clipboard24Regular />} />
+        </Tooltip>
+        <Tooltip content="Paste" relationship="description" withArrow>
+          <ToolbarButton
+            aria-label="Paste"
+            icon={<ClipboardPaste24Regular />}
+          />
+        </Tooltip>
+        <ToolbarDivider />
+        <Tooltip content="Rectangle" relationship="description" withArrow>
+          <ToolbarButton
+            aria-label="Rectangle"
+            icon={<SelectObject24Regular />}
+          />
+        </Tooltip>
+        <Tooltip content="Expand" relationship="description" withArrow>
+          <ToolbarButton aria-label="Expand" icon={<ArrowExpand24Regular />} />
+        </Tooltip>
+        <Tooltip content="Form" relationship="description" withArrow>
+          <ToolbarButton aria-label="Form" icon={<Form24Regular />} />
+        </Tooltip>
+        <Tooltip content="Fill void" relationship="description" withArrow>
+          <ToolbarButton
+            aria-label="Fill void"
+            icon={<ShapeUnion24Regular />}
+          />
+        </Tooltip>
+        <ToolbarDivider />
+        <Tooltip content="Help" relationship="description" withArrow>
+          <ToolbarButton aria-label="Help" icon={<QuestionCircle24Regular />} />
+        </Tooltip>
+      </Toolbar>
+      <Textarea
+        className={styles.textarea}
+        id={textareaId}
+        placeholder='Paint.NET "選択範囲自体をコピー"'
+      />
+    </div>
   );
-}
+};
 
 export default App;
