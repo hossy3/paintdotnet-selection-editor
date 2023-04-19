9048cf2b3c9439fd553f587a043e9a49d077efdf
hossy <hossy@hossy.info>
2023-04-19 22:25:52 +0900

    Update App.tsx
    Support clipboard (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import {
 } from "@fluentui/react-components";
 import {
   ArrowExpand24Regular,
-  Clipboard24Regular,
   ClipboardPaste24Regular,
+  DocumentCopy24Regular,
   Form24Regular,
   QuestionCircle24Regular,
   SelectObject24Regular,
@@ -24,7 +24,7 @@ const useStyles = makeStyles({
     flexDirection: "column",
   },
   textarea: {
-    height: "6em",
+    height: "12em",
   },
 });
 
@@ -32,15 +32,29 @@ const App = () => {
   const textareaId = useId("textarea");
   const styles = useStyles();
 
+  const [selectionText, setSelectionText] = React.useState("");
+
   return (
     <div className={styles.base}>
       <Toolbar aria-label="Default">
         <Tooltip content="Copy" relationship="description" withArrow>
-          <ToolbarButton aria-label="Copy" icon={<Clipboard24Regular />} />
+          <ToolbarButton
+            aria-label="Copy"
+            disabled={selectionText === ""}
+            onClick={() => {
+              navigator.clipboard.writeText(selectionText);
+            }}
+            icon={<DocumentCopy24Regular />}
+          />
         </Tooltip>
         <Tooltip content="Paste" relationship="description" withArrow>
           <ToolbarButton
             aria-label="Paste"
+            onClick={() => {
+              navigator.clipboard.readText().then((text) => {
+                setSelectionText(text);
+              });
+            }}
             icon={<ClipboardPaste24Regular />}
           />
         </Tooltip>
@@ -69,9 +83,14 @@ const App = () => {
         </Tooltip>
       </Toolbar>
       <Textarea
-        className={styles.textarea}
         id={textareaId}
+        onChange={(_, data) => {
+          setSelectionText(data.value);
+        }}
         placeholder='Paint.NET "選択範囲自体をコピー"'
+        resize="vertical"
+        textarea={{ className: styles.textarea }}
+        value={selectionText}
       />
     </div>
   );
