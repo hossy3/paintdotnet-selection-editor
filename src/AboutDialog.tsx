ab459103c565c34854c80d25627ba706def6bab0
hossy <hossy@hossy.info>
2023-04-21 23:20:08 +0900

    Add about dialog

---
+++ src/AboutDialog.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogContent,
+  DialogProps,
+  DialogSurface,
+  DialogTitle,
+  DialogTrigger,
+  Link,
+  Text,
+} from "@fluentui/react-components";
+
+type AboutDialogProps = Pick<DialogProps, "open" | "onOpenChange">;
+
+export const AboutDialog = (props: AboutDialogProps) => {
+  return (
+    <Dialog {...props}>
+      <DialogSurface>
+        <DialogBody>
+          <DialogTitle>Selection Editor for Paint.NET</DialogTitle>
+          <DialogContent>
+            <div>
+              <Text>
+                Paint.NET の選択範囲テキストを編集する WEB アプリケーション
+              </Text>
+            </div>
+            <div>
+              <Link href="https://github.com/hossy3/paintdotnet-selection-editor/">
+                GitHub
+              </Link>
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <DialogTrigger disableButtonEnhancement>
+              <Button appearance="primary">Close</Button>
+            </DialogTrigger>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
+  );
+};
